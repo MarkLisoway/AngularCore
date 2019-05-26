0fc05cf4463edda5a0da7e9d78cf6bc341b52df2
Mark Lisoway <lisowaym@gmail.com>
2019-05-25 18:04:22 -0600

    Copy boilerplate from test project

---
+++ WebApp/Configurations/SpaConfig.cs
@@ -0,0 +1,11 @@
+namespace WebApp.Configurations
+{
+    // Properties are populated through the configuration binder
+    // ReSharper disable once UnusedAutoPropertyAccessor.Global
+    public sealed class SpaConfig
+    {
+        public string SourcePath { get; set; }
+        
+        public string RootPath { get; set; }
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApp.Configurations;
 
 namespace WebApp
 {
@@ -20,12 +17,35 @@ namespace WebApp
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
+
+        private ServiceProvider BindConfigurations(IServiceCollection services)
+        {
+            var spaConfigLocation = Configuration.GetSection("SPA");
+            services.Configure<SpaConfig>(spaConfigLocation);
+
+            return services.BuildServiceProvider();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Return bounded provider to use configurations immediately
+            var serviceProvider = BindConfigurations(services);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // TODO Once app is ready for a full production release, change MaxAge to a much longer time (1+ years)
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromHours(12);
+            });
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = serviceProvider.GetService<IOptions<SpaConfig>>().Value.RootPath;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,12 +57,34 @@ namespace WebApp
             }
             else
             {
+                // TODO Add default exception page that is not related the the SPA app
+                // app.UseExceptionHandler("/Error");
+                
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+            
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    "default",
+                    "{controller}/{action}/{id?}");
+            });
+            
+            app.UseSpa(spa =>
+            {
+                // TODO Link to configurations rather than a string constant
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer("start-dev");
+                }
+            });
         }
     }
 }
